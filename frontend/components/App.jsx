0382d186f8d915e387f927108fc417c8cdea90b7
pratikAgspert <pratik@agspert.com>
2024-11-09 18:11:15 +0530

    Feat: Product Info

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,14 +1,15 @@
-import { Stack } from "@chakra-ui/react";
-import CubeWithLineTexture from "./CubeWithLineTexture";
 import Story from "./Story";
 
 export default function App({ home }) {
   console.log("Home", home);
 
+  const productId = window.productId;
+  const productHandle = window.productHandle;
+
+  console.log("PRODUCT INFO: ", productId, "productHandle", productHandle);
+
   return (
     <div className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-h-screen">
-      {/* <CubeWithLineTexture /> */}
-
       <Story />
     </div>
   );
