78f25b47cfe026f2f39e78d33531928a4c8e28d4
pratikAgspert <pratik@agspert.com>
2024-11-19 16:12:02 +0530

    remove unused info tags

--- web/frontend/components/ProductStoryVisualizer/HeroSection.jsx
+++ web/frontend/components/ProductStoryVisualizer/HeroSection.jsx
@@ -6,15 +6,15 @@ import {
   Stack,
   Text,
   VStack,
-} from '@chakra-ui/react';
-import { DrawerInfo } from './DrawerInfo';
-import { FiExternalLink } from 'react-icons/fi';
-import DraggableDrawer from './generic/DraggableDrawer';
-import { TbView360Number } from 'react-icons/tb';
-import { keyframes } from '@emotion/react';
-import styled from '@emotion/styled';
-import { useEffect, useState } from 'react';
-import ScreenInfoCard from './generic/ScreenInfoCard';
+} from "@chakra-ui/react";
+import { DrawerInfo } from "./DrawerInfo";
+import { FiExternalLink } from "react-icons/fi";
+import DraggableDrawer from "./generic/DraggableDrawer";
+import { TbView360Number } from "react-icons/tb";
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
+import ScreenInfoCard from "./generic/ScreenInfoCard";
 
 const blink = keyframes`
   0% { opacity: 1; }
@@ -36,66 +36,40 @@ export const HeroSection = ({
 }) => {
   // const [showIcon, setShowIcon] = useState(true);
 
-  const imageScreen = data?.find((info) => info?.type === '2d_image');
-  const videoScreen = data?.find((info) => info?.type === '2d_video');
+  const imageScreen = data?.find((info) => info?.type === "2d_image");
+  const videoScreen = data?.find((info) => info?.type === "2d_video");
 
-  const redirect_url = data?.find((info) => info?.type === 'redirect_url');
+  const redirect_url = data?.find((info) => info?.type === "redirect_url");
 
   return (
     <>
       {header ? (
         <Text
-          position={'absolute'}
-          top={'3.5rem'}
-          left={'1.5rem'}
+          position={"absolute"}
+          top={"3.5rem"}
+          left={"1.5rem"}
           paddingX={3}
           borderRadius={100}
           fontWeight={500}
-          bg={'lightcyan'}
+          bg={"lightcyan"}
           fontSize={16}
         >
           {header}
         </Text>
       ) : null}
-      <VStack position={'absolute'} top={10} right={5} spacing={3}>
+      <VStack position={"absolute"} top={10} right={5} spacing={3}>
         <a
-          href={redirect_url ? redirect_url?.link?.url : 'https://agspert.com/'}
+          href={redirect_url ? redirect_url?.link?.url : "https://agspert.com/"}
           target="_blank"
         >
           <IconButton
             icon={<FiExternalLink size={20} />}
-            color={'black'}
+            color={"black"}
             borderRadius={50}
-            bg={'whiteAlpha.800'}
+            bg={"whiteAlpha.800"}
           />
         </a>
       </VStack>
-
-      {isImage && (
-        <Stack
-          position={'absolute'}
-          bottom={imageScreen?.screen_info?.y_axis}
-          left={imageScreen?.screen_info?.x_axis}
-        >
-          <ScreenInfoCard
-            left={imageScreen?.screen_info?.x_axis}
-            data={imageScreen?.screen_info}
-          />
-        </Stack>
-      )}
-
-      {isVideo && (
-        <Stack
-          position={'absolute'}
-          bottom={videoScreen?.screen_info?.y_axis}
-          left={videoScreen?.screen_info?.x_axis}
-        >
-          <ScreenInfoCard
-            left={videoScreen?.screen_info?.x_axis}
-            data={videoScreen?.screen_info}
-          />
-        </Stack>
-      )}
 
       {/* {showIcon && (
         <IconWithAnimation
