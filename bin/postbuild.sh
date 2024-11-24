21da0f5c16aacb3978cd80fca7240f9b5c649ee4
Prince Baghel <pkbghl90@gmail.com>
2024-11-24 23:06:24 +0530

    chore: build

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -3,11 +3,18 @@
 rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute
+mkdir -p ./.amplify-hosting/compute/default
+mkdir -p ./.amplify-hosting/compute/default/frontend
 
-cp -r ./dist ./.amplify-hosting/compute/default
+# cp -r ./dist ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
-cp -r public ./.amplify-hosting/static
-cp -r web/frontend/dist ./.amplify-hosting/compute/default/frontend/build
+cp -r web/frontend/dist ./.amplify-hosting/compute/default/frontend/dist
+cp -r web/index.js ./.amplify-hosting/compute/default/index.js
+# cp -r web/.env ./.amplify-hosting/compute/default/.env
+cp -r web/package.json ./.amplify-hosting/compute/default/package.json
+cp -r web/privacy.js ./.amplify-hosting/compute/default/privacy.js
+cp -r web/shopify.js ./.amplify-hosting/compute/default/shopify.js
+cp -r web/product-creator.js ./.amplify-hosting/compute/default/product-creator.js
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
