63d297b0d70962fbea730b27de6b828a3341beac
Prince Baghel <pkbghl90@gmail.com>
2024-11-24 21:41:17 +0530

    chore: postbuild update

--- postbuild.sh
+++ postbuild.sh
@@ -8,5 +8,6 @@ cp -r ./dist ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
 cp -r public ./.amplify-hosting/static
+cp -r web/frontend/dist ./.amplify-hosting/compute/default/frontend/build
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
