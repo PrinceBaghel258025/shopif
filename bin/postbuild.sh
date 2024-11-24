10df31de088866109232b5f71f0d02e754bd92bb
Prince Baghel <pkbghl90@gmail.com>
2024-11-24 23:53:33 +0530

    chore: build issue

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -6,15 +6,15 @@ mkdir -p ./.amplify-hosting/compute
 mkdir -p ./.amplify-hosting/compute/default
 mkdir -p ./.amplify-hosting/compute/default/frontend
 
-# cp -r ./dist ./.amplify-hosting/compute/default
-cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
+# cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
 cp -r web/frontend/dist ./.amplify-hosting/compute/default/frontend/dist
 cp -r web/index.js ./.amplify-hosting/compute/default/index.js
-# cp -r web/.env ./.amplify-hosting/compute/default/.env
 cp -r web/package.json ./.amplify-hosting/compute/default/package.json
 cp -r web/privacy.js ./.amplify-hosting/compute/default/privacy.js
 cp -r web/shopify.js ./.amplify-hosting/compute/default/shopify.js
 cp -r web/product-creator.js ./.amplify-hosting/compute/default/product-creator.js
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
+cd ./.amplify-hosting/compute/default
+yarn install --frozen-lockfile --production --ignore-scripts --non-interactive
