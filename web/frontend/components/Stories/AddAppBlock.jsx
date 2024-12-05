151a6c6bdf1fc6066bcaa122726fd5d8b92a1f66
pratikAgspert <pratik@agspert.com>
2024-12-05 19:10:34 +0530

    feat: AddAppBlockModal

--- web/frontend/components/Stories/AddAppBlock.jsx
+++ web/frontend/components/Stories/AddAppBlock.jsx
@@ -1,11 +1,30 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../../services/context";
-import { Button, HStack, IconButton, Tooltip } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  Tooltip,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { FiPlusCircle } from "react-icons/fi";
 
 const AddAppBlock = ({ icon = false }) => {
   const { getShop } = useContext(AuthContext);
-  console.log("env var", import.meta.env.MODE , import.meta.env.SHOPIFY_KODEX_EXTENSION, import.meta.env?.VITE_SHOPIFY_KODEX_EXTENSION_ID)
+  console.log(
+    "env var",
+    import.meta.env.MODE,
+    import.meta.env.SHOPIFY_KODEX_EXTENSION,
+    import.meta.env?.VITE_SHOPIFY_KODEX_EXTENSION_ID
+  );
 
   // const appBlockId = "0828473a-1a91-4fa8-a806-7f65c8871ca2";
   const appBlockId = import.meta.env?.VITE_SHOPIFY_KODEX_EXTENSION_ID;
@@ -39,8 +58,56 @@ const AddAppBlock = ({ icon = false }) => {
           </a>
         </HStack>
       )}
+
+      <AddAppBlockModal addExtUrl={addExtUrl} />
     </>
   );
 };
 
 export default AddAppBlock;
+
+export const AddAppBlockModal = ({ addExtUrl }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  useEffect(() => {
+    // Check if the modal has been shown before
+    const hasModalBeenShown = localStorage.getItem("appBlockModalShown");
+
+    // If modal hasn't been shown before, open it and mark as shown
+    if (!hasModalBeenShown) {
+      onOpen();
+      localStorage.setItem("appBlockModalShown", "true");
+    }
+  }, [onOpen]);
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={handleClose} closeOnOverlayClick={false}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Add Extension Block</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text>
+            To access story features, ensure that your theme includes an
+            extension. Without it, these features won't be visible.
+          </Text>
+        </ModalBody>
+
+        <ModalFooter>
+          <a
+            href={addExtUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ width: "fit-content" }}
+          >
+            <Button colorScheme="blue">Add Section</Button>
+          </a>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
